006e5e8d56bed52d3ede5ba43303cf1f2e950f8d
SandeepAcumen <sandeepkumarg.acumenusa@gmail.com>
2025-03-25 09:49:46 +0530

    pushed till Packages

--- 28Inheritance.py
+++ 28Inheritance.py
@@ -12,15 +12,16 @@ class Mammal:
         print("walk")
 
 class Dog(Mammal):
-    def bark(self):
-        print("bark")
+    pass
+    # def bark(self):
+    #     print("bark")
 
 class Cat(Mammal):
     pass
 
 dog1 =Dog()
 dog1.walk()
-dog1.bark()
+
 
 
 
